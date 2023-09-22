1d2853f9b3acf0376844cdaae2b72379cedbf913
xumingw <matthianxu@gmail.com>
2023-09-22 21:28:54 +0800

    Add more description and add seg result files

--- sort.js
+++ sort.js
@@ -11,7 +11,7 @@ function sortTable(n, t, id) {
   table = document.getElementById(id);
   switching = true;
   // Set the sorting direction to ascending:
-  dir = "asc";
+  dir = "desc";
   /* Make a loop that will continue until
 no switching has been done: */
   while (switching) {
@@ -84,8 +84,8 @@ no switching has been done: */
     } else {
       /* If no switching has been done AND the direction is "asc",
     set the direction to "desc" and run the while loop again. */
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
+      if (switchcount == 0 && dir == "desc") {
+        dir = "asc";
         switching = true;
       }
     }
